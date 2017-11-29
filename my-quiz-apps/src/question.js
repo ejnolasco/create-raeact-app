ae128080c7b439bab4052295d129695794f6848c
ej <ej@gmail.ph>
2017-11-28 16:07:36 -0800

    finals

---
+++ my-quiz-apps/src/question.js
@@ -0,0 +1,76 @@
+ var questions = [{
+ 	"question": "BYOD STANDS FOR?",
+ 	"option1": "BINMALEY YOUTH ORGANIZATION DEVELOPMENT",
+ 	"option2": "BIBLICAL YOUNG OR DEFENCE",
+ 	"option3": "BRING YOUR OWN DEVICE",
+ 	"option4": "BRING YOUNG ORW DEVICES",
+ 	"answer":"3"
+
+}, {
+	"question": "HOW TO INSTALL CORDOVA?",
+ 	"option1": "$NPM INSTALL -G GORDOVA",
+ 	"option2": "$NPM INSTALL -G CORDOVA",
+ 	"option3": "$NPM INSTALL -G CRODOVA",
+ 	"option4": "$NPM INSTALL -G CORDOVA@LATEST",
+ 	"answer":"2"
+}, {
+	"question": "HOW TO INSTALL REACTJS?",
+	"option1": "$ npm installest -j create-react-app",
+	"option2": "$ npm installer -g codova-react-app",
+	"option3": "$ npm installing -g create-reactjs-app",
+	"option4": "$ npm install -g create-react-app",
+	"answer": "4"
+
+}, {
+	"question": "HOW TO UPLOAD YOUR FILES N GITHUB?",
+	"option1": "GIT PUSH",
+	"option2": "GET PUSH",
+	"option3": "GIET PUSH",
+	"option4": "GITS PUSH",
+	"answer": "1"
+
+}, {
+	"question": "a free, open source framework for building cross-platform native applications using HTML5",
+	"option1": "APACHE FOUNDATION",
+	"option2": "APACHE CORDOVA ",
+	"option3": "APACHE APPS",
+	"option4": "APACHE FRAMEWORK7",
+	"answer": "2"
+}, {
+	"question": "HOW TO RUN YOUR REACTJS IN CMD?",
+	"option1": "$NPM RUN",
+	"option2": "$NPM GO",
+	"option3": "$NPM BUIILD",
+	"option4": "$NPM START",
+	"answer": "4"
+}, {
+	"question": "how",
+	"option1": "russer westbrook",
+	"option2": "kobe",
+	"option3": "jordan",
+	"option4": "durant",
+	"answer": "1"
+}, {
+	"question": "who is the ccurrent president of the Philippines?",
+	"option1": "noynoy aquino",
+	"option2": "manuel roxas",
+	"option3": "emilio aguinaldo",
+	"option4": "rodrigo duterte",
+	"answer": "4"
+}, {
+	"question": "what is year today ?",
+	"option1": "2020",
+	"option2": "2018",
+	"option3": "2017",
+	"option4": "1",
+	"answer": "3"
+}, {
+	"question": "anu say mo??",
+	"option1": "blog",
+	"option2": "web",
+	"option3": "idk",
+	"option4": "were",
+	"answer": "1"
+}];
+
+
